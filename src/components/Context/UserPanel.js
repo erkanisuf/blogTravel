cd354da004a4f75ac342152766071baf4334ee53
erkanisuf <63913743+erkanisuf@users.noreply.github.com>
2020-09-26 01:16:42 +0300

    userpaneLL

--- src/components/Context/UserPanel.js
+++ src/components/Context/UserPanel.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { BlogContext } from "./BlogContext";
 
 const UserPanel = () => {
@@ -7,7 +7,30 @@ const UserPanel = () => {
   const [useremail, setuserEmail] = valueFour;
   const [userid, setuserId] = valueThree;
   const [favorites, setfavorites] = valueSix;
-  return <div></div>;
+
+  console.log(favorites);
+  console.log(blogs);
+
+  const checkuserAndPost = () => {
+    let arr = blogs;
+    let favK = favorites.find((el) => {
+      return el.id === useremail;
+    });
+
+    const putka = { ...favK };
+    const anusaa = putka.favoritePost;
+    const kurche = [...anusaa];
+    console.log("kurche", kurche);
+
+    const intersection = blogs.filter((element) => kurche.includes(element.id));
+    console.log(intersection);
+  };
+
+  return (
+    <div>
+      <button onClick={checkuserAndPost}>sllslsls</button>
+    </div>
+  );
 };
 
 export default UserPanel;
